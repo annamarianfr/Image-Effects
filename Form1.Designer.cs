b8c1eb00898320eb55b846bff7894d617f7c08ff
annamarianfr <annamarianfr@gmail.com>
2016-05-07 02:00:48 +0300

    Added interface, grayscale, sepia and waves effects

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.source = new System.Windows.Forms.PictureBox();
+            this.result = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.wavesBtn = new System.Windows.Forms.PictureBox();
+            this.sepiaBtn = new System.Windows.Forms.PictureBox();
+            this.grayscaleBtn = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.source)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wavesBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sepiaBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grayscaleBtn)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // source
+            // 
+            this.source.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.source.Location = new System.Drawing.Point(29, 27);
+            this.source.Name = "source";
+            this.source.Size = new System.Drawing.Size(600, 500);
+            this.source.TabIndex = 0;
+            this.source.TabStop = false;
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.result.Location = new System.Drawing.Point(738, 27);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(600, 500);
+            this.result.TabIndex = 1;
+            this.result.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox8);
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.wavesBtn);
+            this.panel1.Controls.Add(this.sepiaBtn);
+            this.panel1.Controls.Add(this.grayscaleBtn);
+            this.panel1.Location = new System.Drawing.Point(29, 561);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1309, 149);
+            this.panel1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(697, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(562, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(425, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Waves";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sepia";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Alb-Negru";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(700, 16);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(120, 100);
+            this.pictureBox8.TabIndex = 5;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(565, 16);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(120, 100);
+            this.pictureBox7.TabIndex = 4;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(428, 16);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(120, 100);
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            // 
+            // wavesBtn
+            // 
+            this.wavesBtn.Location = new System.Drawing.Point(289, 16);
+            this.wavesBtn.Name = "wavesBtn";
+            this.wavesBtn.Size = new System.Drawing.Size(120, 100);
+            this.wavesBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.wavesBtn.TabIndex = 2;
+            this.wavesBtn.TabStop = false;
+            this.wavesBtn.Click += new System.EventHandler(this.wavesBtn_Click);
+            // 
+            // sepiaBtn
+            // 
+            this.sepiaBtn.Location = new System.Drawing.Point(151, 16);
+            this.sepiaBtn.Name = "sepiaBtn";
+            this.sepiaBtn.Size = new System.Drawing.Size(120, 100);
+            this.sepiaBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.sepiaBtn.TabIndex = 1;
+            this.sepiaBtn.TabStop = false;
+            // 
+            // grayscaleBtn
+            // 
+            this.grayscaleBtn.Location = new System.Drawing.Point(14, 16);
+            this.grayscaleBtn.Name = "grayscaleBtn";
+            this.grayscaleBtn.Size = new System.Drawing.Size(120, 100);
+            this.grayscaleBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.grayscaleBtn.TabIndex = 0;
+            this.grayscaleBtn.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.saveImageToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1350, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // saveImageToolStripMenuItem
+            // 
+            this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.saveImageToolStripMenuItem.Text = "Save Image";
+            this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.saveImageToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.aboutToolStripMenuItem.Text = "Author";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(1350, 722);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.source);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.source)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wavesBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sepiaBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grayscaleBtn)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox source;
+        private System.Windows.Forms.PictureBox result;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox wavesBtn;
+        private System.Windows.Forms.PictureBox sepiaBtn;
+        private System.Windows.Forms.PictureBox grayscaleBtn;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,157 @@ namespace ImageEffects
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            source.Image = ImageEffects.Properties.Resources.Lenna;
+            loadBtns();
+        }
+        private void GrayscaleEffect(PictureBox source, PictureBox destination, bool preview)
+        {
+            if (source.Image != null)
+            {
+                Bitmap image = (Bitmap)source.Image.Clone();
+                Bitmap grayScale = null;
+                if (preview == true)
+                    grayScale = new Bitmap(image, new Size(image.Width / 4, image.Height / 4));
+                else
+                    grayScale = image;
+                int height = grayScale.Size.Height;
+                int width = grayScale.Size.Width;
+                for (int yCoordinate = 0; yCoordinate < height; yCoordinate++)
+                {
+                    for (int xCoordinate = 0; xCoordinate < width; xCoordinate++)
+                    {
+                        Color color = grayScale.GetPixel(xCoordinate, yCoordinate);
+                        int grayColor = (color.R + color.G + color.B) / 3;
+                        grayScale.SetPixel(xCoordinate, yCoordinate, Color.FromArgb(grayColor, grayColor, grayColor));
+                    }
+                }
+                destination.Image = grayScale;
+            }
+        }
+
+        private void SepiaEffect(PictureBox source, PictureBox destination, bool preview)
+        {
+            if (source.Image != null)
+            {
+                Bitmap image = (Bitmap)source.Image.Clone();
+                Bitmap sepia = null;
+                if(preview == true)
+                    sepia = new Bitmap(image, new Size(image.Width/4,image.Height/4));
+                else 
+                    sepia = image;
+                int height = sepia.Size.Height;
+                int width = sepia.Size.Width;
+                for (int yCoordinate = 0; yCoordinate < height; yCoordinate++)
+                {
+                    for (int xCoordinate = 0; xCoordinate < width; xCoordinate++)
+                    {
+                        Color color = sepia.GetPixel(xCoordinate, yCoordinate);
+                        double grayColor = ((double)(color.R + color.G + color.B)) / 3.0d;
+                        Color sepiaColor = Color.FromArgb((byte)grayColor, (byte)(grayColor * 0.95), (byte)(grayColor * 0.82));
+                        sepia.SetPixel(xCoordinate, yCoordinate, sepiaColor);
+                    }
+                }
+                destination.Image = sepia;
+            }
+        }
+
+        private void WavesEffect(PictureBox source, PictureBox destination, bool preview, int distorsion)
+        {
+            if (source.Image != null)
+            {
+                Bitmap image = (Bitmap)source.Image.Clone();
+                Bitmap waves = null;
+                if (preview == true)
+                {
+                    waves = new Bitmap(image, new Size(image.Width / 4, image.Height / 4));
+                    image = waves;
+                }
+                else
+                    waves = image;
+                int height = waves.Size.Height;
+                int width = waves.Size.Width;
+                for (int yCoordinate = 0; yCoordinate < height; yCoordinate++)
+                {
+                    for (int xCoordinate = 0; xCoordinate < width; xCoordinate++)
+                    {
+                        int xo = (int)(distorsion * Math.Sin(2 * Math.PI * yCoordinate / distorsion));
+                        int yo = (int)(distorsion * Math.Cos(2 * Math.PI * xCoordinate / distorsion));
+                        xo += xCoordinate;
+                        yo += yCoordinate;
+                        xo = checkCoordinate(xo, width);
+                        yo = checkCoordinate(yo, height);
+                        Color color = image.GetPixel(xo, yo);
+                        waves.SetPixel(xCoordinate, yCoordinate, color);
+                    }
+                }
+                destination.Image = waves;
+            }
+        }
+
+        private int checkCoordinate(int coord, int limit)
+        {
+            if (coord >= limit)
+                return limit-1;
+            else if (coord < 0)
+                return 0;
+            else
+                return coord;
+        }
+
+        private void loadBtns()
+        {
+            GrayscaleEffect(source, grayscaleBtn, true);
+            SepiaEffect(source, sepiaBtn, true);
+            WavesEffect(source, wavesBtn, true, 2);
+        }
+
+        private void saveImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (result.Image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Images|*.bmp;*.jpg";
+                ImageFormat format = ImageFormat.Bmp;
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string ext = System.IO.Path.GetExtension(sfd.FileName);
+                    switch (ext)
+                    {
+                        case ".jpg":
+                            format = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            format = ImageFormat.Bmp;
+                            break;
+                    }
+                    result.Image.Save(sfd.FileName, format);
+                }
+            }
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "Image files (*.jpg, *.jpeg, *.bmp) | *.jpg; *.jpeg; *.bmp";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    source.Image = new Bitmap(dlg.FileName);
+                    result.Image = null;
+                    loadBtns();
+                }
+            }
+        }
+
+        private void wavesBtn_Click(object sender, EventArgs e)
+        {
+            WavesEffect(source, result, false, 10);
         }
     }
 }
